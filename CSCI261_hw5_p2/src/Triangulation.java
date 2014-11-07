72c4a250963b723c24019743d9456132a135dcbf
Derek Brown <djb3718@rit.edu>
2014-11-07 14:56:37 -0500

    Created initial java file and added to Eclipse

---
+++ CSCI261_hw5_p2/src/Triangulation.java
@@ -0,0 +1,23 @@
+/**
+ * Triangulation.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ *
+ * Purpose:	Find the minimum possible length of triangulation for a convex
+ * 			polygon.
+ */
+
+import java.util.Scanner;
+
+public class Triangulation {
+	
+	// Attributes
+	
+	// Constructor
+	
+	// Methods
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end Triangulation
