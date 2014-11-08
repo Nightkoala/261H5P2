61d3d3fca09cd179d52beda5cd792e0645c0cea3
Nightkoala <djb3718@rit.edu>
2014-11-07 21:41:54 -0500

    Fixed formatting issues caused by transfer of file to CS machine.  Problem submitted.

--- CSCI261_hw5_p2/src/Triangulation.java
+++ CSCI261_hw5_p2/src/Triangulation.java
@@ -4,7 +4,7 @@
  * @author	Derek Brown <djb3718@rit.edu>
  *
  * Purpose:	Find the minimum possible length of triangulation for a convex
- * 			polygon.
+ * 		polygon.
  */
 
 import java.util.Scanner;
@@ -21,14 +21,14 @@ public class Triangulation {
 	// Constructor
 	
 	/**
-	 * Constructor for creating a Triangulation instance, used to store useful
-	 * information needed for completing the algorithm, such as, the number
-	 * of data points, the x-coordinates, the y-coordinates, and the solution
-	 * array.
+	 * Constructor for creating a Triangulation instance, used to store 
+	 * useful information needed for completing the algorithm, such as,
+	 * the number of data points, the x-coordinates, the y-coordinates,
+	 * and the solution array.
 	 * 
 	 * @param size	The number of vertices
-	 * @param x		The array containing all of the x-coordinates
-	 * @param y		The array containing all of the y-coordinates
+	 * @param x	The array containing all of the x-coordinates
+	 * @param y	The array containing all of the y-coordinates
 	 */
 	public Triangulation( int size, double[] x, double[] y ) {
 		this.size = size;
@@ -49,15 +49,15 @@ public class Triangulation {
 	 * @param j	The j-th vertex
 	 * 
 	 * @return	0, if j-i < 2
-	 * 			length of the line segment connecting the two vertices, 
-	 * 			otherwise.
+	 * 		length of the line segment connecting the two vertices, 
+	 * 		otherwise.
 	 */
 	public double dLength( int i, int j ) {
 		if( j-i >= 2 ) {
-			return Math.sqrt( Math.pow(x[i-1]-x[j-1], 2) + Math.pow(y[i-1]-y[j-1], 2) );
+			return Math.sqrt( Math.pow( x[i-1] - x[j-1], 2 ) +
+				Math.pow( y[i-1] - y[j-1], 2 ) );
 		}//end if
 		return 0;
-
 	}//end calculateSegmentLength
 	
 	/**
@@ -65,11 +65,12 @@ public class Triangulation {
 	 */
 	public void triangulation() {
 		for( int l = 2 ; l <= size ; l++ ) {
-			for( int i = 1 ; i <= size-l+1 ; i++ ) {
+			for( int i = 1 ; i <= size - l + 1 ; i++ ) {
 				int j = i + l - 1;
 				S[i][j] = Double.MAX_VALUE;
 				for( int k = i ; k <= j - 1 ; k++ ) {
-					double temp = S[i][k] + S[k+1][j] + dLength( i, k ) + dLength( k, j );
+					double temp = S[i][k] + S[k+1][j] +
+					dLength( i, k ) + dLength( k, j );
 					if( temp < S[i][j] ) {
 						S[i][j] = temp;
 					}//end if
@@ -81,12 +82,12 @@ public class Triangulation {
 	/**
 	 * The main logic for the program, reads in data from the user, creates
 	 * an object to store the information and then begins the algorithm, the
-	 * algorithm fills in the solution array and upon completion displays the
-	 * result to the user.
+	 * algorithm fills in the solution array and upon completion displays
+	 * the result to the user.
 	 * 
 	 * @param args	Command line arguments, unused.
 	 */
-	public static void main(String[] args) {
+	public static void main( String[] args ) {
 		Scanner sc = new Scanner( System.in );
 		String input;
 		input = sc.next();
@@ -106,6 +107,6 @@ public class Triangulation {
 		sc.close();
 		Triangulation T = new Triangulation( numVals, x, y );
 		T.triangulation();
-		System.out.printf("%.4f\n", T.S[1][T.size]);
+		System.out.printf( "%.4f\n", T.S[1][T.size] );
 	}//end main
 }//end Triangulation
